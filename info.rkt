d0b1f750db7d4ce4a84e0d4f6db254d9950a27f3
Yanying Wang <yanyingwang1@gmail.com>
2023-08-06 19:41:09 +0800

    update for saving code

--- info.rkt
+++ info.rkt
@@ -1,9 +1,16 @@
 #lang info
 (define collection "yjstock")
-(define deps '("base"))
+(define deps
+  '("base"
+    "html-parsing"
+    "at-exp-lib"
+    "gregor-lib"
+    "https://github.com/yanyingwang/smtp.git"
+    "https://github.com/yanyingwang/http-client.git"
+    "https://github.com/yanyingwang/timable.git"
+    ))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/yjstock.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
-(define pkg-authors '(yanying))
-(define license '(Apache-2.0 OR MIT))
+(define pkg-desc "yijing symbols stock chart")
+(define version "0.1")
+(define pkg-authors '("Yanying Wang"))
--- main.rkt
+++ main.rkt
@@ -1,4 +1,4 @@
-#lang racket/base
+#lang at-exp racket/base
 
 (module+ test
   (require rackunit))
@@ -25,6 +25,75 @@
 
 ;; Code here
 
+
+
+(require ming
+         ming/number
+
+         )
+
+
+
+(名 顶价
+    (𡊤 􏺗 (佫 (入 (e) (句化米 (􏿰弔 e 'high))) 文)))
+(名 底价
+    (𡊤 􏺘 (佫 (入 (e) (句化米 (􏿰弔 e 'low))) 文)))
+
+(名 顶量
+    (𡊤 􏺗 (佫 (λ (e) (句化米 (􏿰弔 e 'volume))) 文)))
+(名 底量
+    (𡊤 􏺘 (佫 (入 (e) (句化米 (􏿰弔 e 'volume))) 文)))
+
+
+
+
+
+(define xpage
+  `(html
+    (head
+     (title @,~a{YJStock - @(~t (now #:tz "Asia/Shanghai") "yyyy-MM-dd HH:mm")})
+     (meta ((name "viewport") (content "width=device-width, initial-scale=0.9")))
+     ;; (style
+     ;;     "body { background-color: linen; } .main { width: auto; padding-left: 10px; padding-right: 10px; } .row { padding-top: 10px; } .text { padding-left: 30px; } .subtext { padding-left: 30px; font-size: 90%; } h2 { margin-bottom: 6px; } p { margin-top: 6px; } .responsive { width: 100%; height: auto; }")
+          )
+    (body
+     (div ((class "main"))
+          (div
+               (h1 "新冠肺炎报告")
+               (p ((class "subtext"))
+                  #;"作者：Yanying"
+                  #;(br)
+                  "数据来源：QQ/Sina"
+                  (br)
+                  @,~a{更新日期：@(~t (now #:tz "Asia/Shanghai") "yyyy-MM-dd HH:mm")}
+                  #;(br)
+                  #;(a ((href "https://www.yanying.wang/daily-report")) "原连接")
+                  #;(entity 'nbsp)
+                  #;(a ((href "https://github.com/yanyingwang/daily-report")) "源代码")
+))
+          (div ((class "text"))
+               ,(div-wrap processed/domestic/overall)
+               #;,(div-wrap processed/domestic/overall1))
+          ,(div-wrap/+img processed/domestic/top10 domestic.jpeg)
+          ,(div-wrap/+img processed/foreign/conadd/top10 foreign-conadd.jpeg)
+          ,(div-wrap/+img processed/foreign/deathadd/top10 foreign-deathadd.jpeg)
+          ,(div-wrap/+img processed/foreign/connum/top10 foreign-connum.jpeg)
+          ,(div-wrap/+img processed/foreign/deathnum/top10 foreign-deathnum.jpeg)
+               ))))
+(define xpage/string (xexpr->string xpage))
+
+`
+(名 沪市A股 '("600" "601" "603"))
+(名 沪市B股 '("900"))
+
+(名 深市A股 '("000"))
+(名 深市B股 '("200"))
+
+(名 科创板 '("688"))
+(名 创业板 '("300"))
+(名 中小板 '("002"))
+
+(名 早期上市股 '("6006"))
 
 
 (module+ test
