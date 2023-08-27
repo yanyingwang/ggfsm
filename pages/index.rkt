eb5f6a3fcf70ecc8d66b58ae08390f98c3b23598
Yanying Wang <yanyingwang1@gmail.com>
2023-08-27 17:57:41 +0800

    enh index.html

--- pages/index.rkt
+++ pages/index.rkt
@@ -10,40 +10,44 @@
 (provide index.html)
 
 
-(名 AL1
-    (􏿳 "603259" '药明康德 "300015" '艾尔眼科 "600750" '江中药业 "600559" '老白干 "000858" '五粮液 "000568" '泸州老窖))
+(名 AL
+    (􏿳 "603259" '药明康德 "300015" '艾尔眼科 "600750" '江中药业 "600559" '老白干 "000858" '五粮液 "000568" '泸州老窖
+        "600519" '贵州茅台 "002049" '紫光国微 "300750" '宁德时代 "002594" '比亚迪 "002709" '天赐材料 "002460" '赣锋锂业
+        ))
 
-(名 AL2
-    (􏿳 "600519" '贵州茅台 "002049" '紫光国微 "300750" '宁德时代 "002594" '比亚迪 "002709" '天赐材料 "002460" '赣锋锂业))
 
-
-(名 (tda L)
-    `(td (a ([href ,(~a (阳 L) ".html")]) ,(~a (阴 L)))))
+(名 (自选 t)
+    `(div ((class "mt-5 border"))
+      (h2 ((class "py-2 border-bottom")) ,t)
+          (div ([class "row justify-content-center"])
+               ,@(佫 (λ (P) `(div ([class "col-2 my-2"])
+                                  (a ([class "link-underline-light"] [href ,(~a (阳 P) ".html")]) ,(~a (阴 P)))
+                                  ))
+                     AL)
+               ))
+    )
 
 (名 page
     (wrapped
      "索引"
      `(div ([class "container my-5 text-center"])
-           ,(h1 "索引")
+           (h1 ((class "pt-3 pb-1 border-bottom")) "索引")
            (div ([class "row justify-content-center"])
-                (div ([class "col-8"])
-                     (table ([class "table text-center"])
-                            (tbody
-                             (tr
-                              (td (a ([href "hs300.html"]) "沪深300"))
-                              (td (a ([href "zz500.html"]) "中证500"))
-                              (td (a ([href "sz50.html"]) "上证50"))
-                              (td (a ([href "sc500.html"]) "深成500"))
-                              )
-                             )))
-                (div ([class "width-100"]))
-                (div ([class "col"])
-                     (table ([class "table text-center"])
-                            (tbody
-                             (tr ,@(佫 tda AL1))
-                             (tr ,@(佫 tda AL2))
-                             )))
-                )
+                (div ([class "col-2 py-2 border-bottom"])
+                     (a ([class " link-underline-light"] [href "hs300.html"]) "沪深300"))
+                (div ([class "col-2 py-2 border-bottom"])
+                     (a ([class " link-underline-light"] [href "zz500.html"]) "中证500"))
+                (div ([class "col-2 py-2 border-bottom"])
+                     (a ([class " link-underline-light"] [href "sz50.html"]) "上证50"))
+                (div ([class "col-2 py-2 border-bottom"])
+                     (a ([class " link-underline-light"] [href "sc500.html"]) "深成500")))
+           (div ((class "mt-5 border"))
+                (h2 ((class "py-2 border-bottom")) "自选1")
+                (div ([class "row justify-content-center"])
+                     (div ([class "col-2 my-2"])
+                          (a ([class "link-underline-light"] [href "#"]) "请添加自选"))
+                     ))
+           ,(自选 "自选2")
            ))
     )
 
