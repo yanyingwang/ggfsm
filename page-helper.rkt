7ad6d4838a56f18d951713cd8ff6f41e827e032c
Yanying Wang <yanyingwang1@gmail.com>
2025-06-14 00:23:18 +0800

    reset to ggfsm

--- page-helper.rkt
+++ page-helper.rkt
@@ -55,10 +55,7 @@
                   (div ([class "row footer-top"] [style "text-align: center;"]))
                   (div ([class "row text-center justify-content-center"])
                        (div ([class "col-sm-3 col-md-3 col-lg-3"])
-                            (p ([style "color: gray;"]) "© wwww.yanying.wang. All rights reserved.")
-                            (p ([style "color: gray;"]) "© 王彦迎. 版权所有."))))))
-
-
+                            (p ([style "color: gray;"]) "© 2025 wwww.yanying.wang. All rights reserved."))))))
 
 
 (名 topbar-input-options
@@ -70,7 +67,7 @@
 (名 topnavs
     `(nav ((class "navbar bg-primary-subtle navbar-expand-lg bg-body-tertiary"))
           (div ((class "container"))
-               (a ((class "navbar-brand") (href "index.html")) "西方财富")
+               (a ((class "navbar-brand") (href "index.html")) "GGFSM")
                (button ((aria-controls "navbarSupportedContent") (aria-expanded "false") (aria-label "Toggle navigation")
                                                                  (data-bs-target "#navbarSupportedContent") (data-bs-toggle "collapse")
                                                                  (class "navbar-toggler") (type "button"))
